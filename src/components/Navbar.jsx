3b0286b5f36262d835b22493bfb852cc128cb429
AlexMuia31 <alexmuia31@gmail.com>
2024-01-10 19:18:29 +0300

    link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,31 @@
 import { AppBar, Box, Container, Toolbar } from "@mui/material";
 import React from "react";
 import { Header, SubHeader } from "./CustomTypo";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
-    <AppBar sx={{background:'#F5F8FB'}}>
+    <AppBar sx={{ background: "#F5F8FB" }}>
       <Toolbar>
         <Container maxWidth="xl">
-          <Box sx={{display:'flex', alignItems:'center',gap:20}}>
+          <Box sx={{ display: "flex", alignItems: "center", gap: 20 }}>
             <Header sx={{ fontSize: { md: "48px", xs: "12px", sm: "48px" } }}>
               Benglo
             </Header>
-            <Box sx={{display:{xs:'none', md:"flex"}, justifyContent:'space-between',width:'100%'}}>
-              <SubHeader>Home</SubHeader>
+            <Box
+              sx={{
+                display: { xs: "none", md: "flex" },
+                justifyContent: "space-between",
+                width: "100%",
+              }}
+            >
+              <Link href="/" style={{ textDecoration: "none" }}>
+                <SubHeader>Home</SubHeader>
+              </Link>
               <SubHeader>About</SubHeader>
-              <SubHeader>Services</SubHeader>
+              <Link href="/services" style={{ textDecoration: "none" }}>
+                <SubHeader>Services</SubHeader>
+              </Link>
               <SubHeader>Contact</SubHeader>
             </Box>
           </Box>
