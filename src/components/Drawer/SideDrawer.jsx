ace9531818998ff6ae264d4194e9cbc5b1361351
AlexMuia31 <alexmuia31@gmail.com>
2024-01-16 12:15:51 +0300

    drawer

--- src/components/Drawer/SideDrawer.jsx
+++ src/components/Drawer/SideDrawer.jsx
@@ -1,5 +1,8 @@
-import { Box, Drawer } from "@mui/material";
+import { Box, Drawer, Icon, IconButton } from "@mui/material";
 import React from "react";
+import { SubHeader } from "../CustomTypo";
+import CloseIcon from "@mui/icons-material/Close";
+import Link from "next/link";
 
 const SideDrawer = ({ setOpen, open }) => {
   const handleDrawerClose = () => {
@@ -8,13 +11,55 @@ const SideDrawer = ({ setOpen, open }) => {
   return (
     <Box>
       <Drawer
-        sx={{}}
         variant="temporary"
         anchor="top"
         open={open}
         onClose={handleDrawerClose}
       >
-        test
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "flex-end",
+            mr: "20px",
+            mt: "20px",
+          }}
+        >
+          <IconButton onClick={handleDrawerClose}>
+            <CloseIcon />
+          </IconButton>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            flexDirection: "column",
+            gap: 5,
+            p: "10%",
+          }}
+        >
+          <Link
+            href="/"
+            style={{ textDecoration: "none" }}
+            onClick={handleDrawerClose}
+          >
+            <SubHeader>Home</SubHeader>
+          </Link>
+          <Link
+            href="/about"
+            style={{ textDecoration: "none" }}
+            onClick={handleDrawerClose}
+          >
+            <SubHeader>About</SubHeader>
+          </Link>
+          <Link
+            href="/portfolio"
+            style={{ textDecoration: "none" }}
+            onClick={handleDrawerClose}
+          >
+            <SubHeader>Portfolio</SubHeader>
+          </Link>
+        </Box>
       </Drawer>
     </Box>
   );
